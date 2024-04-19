3ccea30894361be24c11ebeec2ef7629fcea4a07
StolinskiJan <147153943+StolinskiJan@users.noreply.github.com>
2024-04-19 12:01:11 +0200

    Add files via upload

---
+++ Prog_aplik_int_19.04.2024/scripts/data.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>Dane z formularza</h3>
+    <?php
+    //echo $_POST['firstName'];
+    //if (isset($_POST)) {
+    if (!empty($_POST['firstName']) && !empty($_POST['pass'])) {
+        echo "Dane:<br>";
+        foreach ($_POST as $data) {
+            echo "$data<br>";
+        }
+
+        echo"<hr>";
+        echo'<pre>';
+        print_r($_POST);
+        echo'</pre>';
+    }else{
+        //echo "Brak danych";
+        echo "<script>history.back();</script>";
+    }
+    
+    ?>
+</body>
+</html>
