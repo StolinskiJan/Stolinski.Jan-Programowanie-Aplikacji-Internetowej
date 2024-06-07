447d28161a7861e5f9736f40d0d2a64569c79392
StolinskiJan <147153943+StolinskiJan@users.noreply.github.com>
2024-06-07 12:12:19 +0200

    Delete Prog_aplik_int_07.06.2024 directory

--- Prog_aplik_int_07.06.2024/app/config/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-    return [
-        'host' => 'localhot',
-        'db_name' => 'wsb',
-        'user_name' => 'root',
-        'password' => '',
-        'charset' => 'utf8',
-    ];
--- Prog_aplik_int_07.06.2024/public/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-    define('BASE_PATH', realpath(dirname(__FILE__).'/..'));
-    require_once BASE_PATH . '/app/controllers/UserController.php';//../ jedno wyżej
-
-    $userController = new UserController();
-
-    if ($_SERVER['REQUEST_URI'] == ) // dokończyć
-?>
